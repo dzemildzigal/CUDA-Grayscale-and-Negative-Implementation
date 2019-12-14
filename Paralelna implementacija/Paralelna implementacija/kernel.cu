0f69ea5a7aad3dff20bc783c2ab6ad136a9f5f25
Džemil Džigal <ddzigal1@etf.unsa.ba>
2019-12-14 17:05:32 +0100

    dodavanje paralelne implementacije, grayscale filter zavrsen (sekvencijalna i paralelna implementacija)

--- Paralelna implementacija/Paralelna implementacija/kernel.cu
+++ Paralelna implementacija/Paralelna implementacija/kernel.cu
@@ -1,30 +1,82 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
 #include <stdio.h>
-
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ 
-void grayScale(const uchar4* rgbImage, unsigned char* grayscaleImage, const int height, const int width) {
-	int x = blockDim.x * blockDim.x + threadIdx.x; // dobijanje x koordinate piksela
-	int y = blockDim.y * blockDim.y + threadIdx.y; // dobijanje y koordinate piksela
-	if (x >= width || y >= height) return; // pregled da li je vrijednost proslijedjena funkciji unutar slike
-	int pixelIndex = width * y + x;
-	grayscaleImage[pixelIndex] = 0.299f * rgbImage[pixelIndex].x + 0.587f * rgbImage[pixelIndex].y + 0.114f * rgbImage[pixelIndex].z;
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <device_launch_parameters.h>
+#include <time.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "C://Users/Jemo/source/repos/Paralelna implementacija/Paralelna implementacija/stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "C://Users/Jemo/source/repos/Paralelna implementacija/Paralelna implementacija/stb_image/stb_image_write.h"
+void getError(cudaError_t err) {
+	if (err != cudaSuccess) {
+		printf("Greska");
+	}
 }
 
+__global__
+void filter(unsigned char* input_rgb, unsigned char* input_gray, int width, int height) {
+	const unsigned int offset = blockIdx.x*blockDim.x + threadIdx.x;
+	const unsigned int a = (offset << 1) + (offset << 0);
+	if (offset < width*height)
+		input_gray[offset] = (input_rgb[a] + input_rgb[a + 1] + input_rgb[a + 2]) * .33333333333333;
+}
 
-int main()
-{
-	uchar4 *rgbimg=0;
-	unsigned char *grayimg=0;
-	int width=0, height=0, channels = 0;
-	const int BLOCK_SZ = 128;
-	const dim3 blockSz(BLOCK_SZ, BLOCK_SZ, 1);
-	const dim3 gridSz(width / BLOCK_SZ + 1, height / BLOCK_SZ + 1, 1);
-	grayScale <<<gridSz, blockSz >>> (rgbimg, grayimg, height, width);
-	printf("Hello world");
-    return 0;
+int main(void) { 
+	unsigned char i = 1;
+	double time = 0.0;
+	char naziv[6] = "a.jpg"; 
+	naziv[5] = '\0';
+	char izlaz[7] = "ao.jpg"; 
+	izlaz[6] = '\0';
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	unsigned char *input, *output;
+	for (i = 1; i <= 8; ++i) {
+		naziv[0] = i + 48;
+
+		int width, height, channels;
+
+		input= stbi_load(naziv, &width, &height, &channels, 0);
+		size_t img_size = width * height * channels;
+		int gray_channels = channels == 4 ? 2 : 1;
+		size_t gray_img_size = width * height * gray_channels;
+		unsigned char *gray_img = (unsigned char*)malloc(gray_img_size);
+		unsigned char *dev_input_RGB, *dev_input_GRAY;
+		
+		getError(cudaMalloc((void**)&dev_input_RGB, img_size * sizeof(unsigned char)));
+		getError(cudaMemcpy(dev_input_RGB, input, img_size * sizeof(unsigned char), cudaMemcpyHostToDevice));
+
+		getError(cudaMalloc((void**)&dev_input_GRAY, gray_img_size * sizeof(unsigned char)));
+		getError(cudaMemcpy(dev_input_GRAY, gray_img, gray_img_size * sizeof(unsigned char), cudaMemcpyHostToDevice));
+
+
+
+		dim3 blockDims(128, 1, 1);
+		dim3 gridDims((unsigned int)ceil((double)(width*height * 3 / blockDims.x)), 1, 1);
+		
+		cudaEventRecord(start);
+		 // pozvati kernel ovdje
+		filter <<<gridDims, blockDims >>> (dev_input_RGB,dev_input_GRAY, width, height);
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		float milis = 0;
+		cudaEventElapsedTime(&milis, start, stop);
+		time += milis;
+
+
+		getError(cudaMemcpy(gray_img, dev_input_GRAY, gray_img_size * sizeof(unsigned char), cudaMemcpyDeviceToHost));
+		getError(cudaFree(dev_input_GRAY));
+		getError(cudaFree(dev_input_RGB));
+		izlaz[0] = naziv[0];
+		stbi_write_jpg(izlaz, width, height, 1, gray_img, 100);
+	}
+	printf("ukupno %f ms \n", time);
+	return 0; 
 }
--- VS/PRS/SekvencijalnaImplementacija.c
+++ VS/PRS/SekvencijalnaImplementacija.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
+#include <time.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include "../PRS/stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "../PRS/stb_image/stb_image_write.h"
 
 int main(int argc, char const *argv[])
 {
+	double rez = 0;
 	unsigned char i = 1;
 	char naziv[6] = "a.jpg\0";
 	char izlaz[7] = "ao.jpg\0";
+	clock_t begin, end;
 	for (i = 1; i <= 8; i++) {
 		naziv[0] = i+48;
 		int width, height, channels;
-		printf("%s", &naziv);
+		//printf("%s", &naziv);
 		unsigned char *img = stbi_load(naziv, &width, &height, &channels, 0);
 		if (img == NULL) {
 			printf("Greska pri ucitavanju slike.\n");
@@ -31,15 +34,20 @@ int main(int argc, char const *argv[])
 			printf("Nema dovoljno memorije za grayscale sliku.\n");
 			exit(1);
 		}
-
+		begin = clock();
 		for (unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
 			*pg = (uint8_t)((*p + *(p + 1) + *(p + 2)) / 3.0);
 			if (channels == 4) {
 				*(pg + 1) = *(p + 3);
 			}
 		}
+		end = clock();
+		double time = (double)(end - begin)*1000 / CLOCKS_PER_SEC;
+		//printf("\n Vrijeme izvrsavanja programa je: %f s\n", time);
+		rez += time;
 		izlaz[0] = naziv[0];
 		stbi_write_jpg(izlaz, width, height, gray_channels, gray_img, 100);
 	}
+	printf("ukupno %f ms \n", rez);
 	return 0;
 }
